eaca90fba6a8992ca267332bb58e5af02c7e4d18
Ken <opiana.218936@novaliches.sti.edu.ph>
2026-05-11 07:35:27 +0800

    Changes to Cards

--- characters/warrior/cards/warrior_threat_scan.gd
+++ characters/warrior/cards/warrior_threat_scan.gd
@@ -5,6 +5,7 @@ const WEAKENED_STATUS := preload("res://statuses/weakened.tres")
 var weak_duration := 1
 
 func apply_effects(targets: Array[Node], _modifiers: ModifierHandler):
+	@warning_ignore("shadowed_variable_base_class")
 	for target in targets:
 		var status_effect := StatusEffect.new()
 		var weak := WEAKENED_STATUS.duplicate()
