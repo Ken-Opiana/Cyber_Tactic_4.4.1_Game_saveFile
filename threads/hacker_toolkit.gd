77fda1b18570b5602896728abefef36838e6db40
Ken <opiana.218936@novaliches.sti.edu.ph>
2026-05-14 15:19:41 +0800

    Update warrior cards

---
+++ threads/hacker_toolkit.gd
@@ -0,0 +1,11 @@
+extends ThreadPassive
+
+
+func activate_relic(owner: ThreadUI):
+	await Events.player_hand_drawn
+	var player := owner.get_tree().get_first_node_in_group("player") as Player
+	if player:
+		var card_draw_effect := CardDrawEffect.new()
+		card_draw_effect.cards_to_draw = 2
+		card_draw_effect.execute([player])
+		owner.flash()
